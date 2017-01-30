55ab854296c7d780ab25b46f888f5009d9647458
ryotamac <tennis.suta6@gmail.com>
2017-01-31 01:46:12 +0900

    テストの実行環境

--- src/spec/component/Hello.spec.ts
+++ src/spec/component/Hello.spec.ts
@@ -2,5 +2,5 @@ import test from 'ava';
 import Hello from '../../main/component/Hello';
 
 test("Hello static fun", (t) => {
-  t.same(Hello.World(), "World")
+  t.deepEqual(Hello.World(), "World")
 });
